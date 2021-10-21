258f3560d658ecdda9f77903320be62b6646202e
robby <robby902553@gmail.com>
2021-10-21 20:07:33 +0800

    更新首頁畫面

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <div class="item-row">
-      <div class="col-lg-6">
+      <!--<div class="col-lg-6">
         <div class="news text-center">
           <span class="title">公告</span>
           <div class="content">
@@ -35,13 +35,43 @@
             </table>
           </div>
         </div>
-      </div>
-      <div class="col-lg-6">
+      </div>-->
+      <div class="col-lg-8">
         <div class="calender text-center">
           <span class="title">派車行事曆</span>
           <div class="content">
             <FullCalendar :options="calendarOptions" :events="events" />
           </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="news left_wrap">
+          <div class="toptitle d-flex align-items-center">
+            <i class="fas fa-bullhorn mr-2"></i>最新消息
+            <!--<div class="right d-flex align-items-stretch">
+              <a href="#" target="_self" style="color: black; font-size: 8px;">篩選</a>
+            </div>-->
+          </div>
+          <ul>
+            <li>
+              <a href="https://ers.stsp.gov.tw/news/82">
+                <span class="badge badge-warning mr-1">消息</span>
+                <div class="top d-flex justify-content-start">
+                  <div class="title">轉知臺南市政府第一劑BNT疫苗預約接種資訊</div>
+                </div>
+                <div class="bottom">2021-10-21 09:13:58</div>
+              </a>
+            </li>
+            <li>
+              <a href="https://ers.stsp.gov.tw/news/81">
+                <span class="badge badge-warning mr-1">消息</span>
+                <div class="top d-flex justify-content-start">
+                  <div class="title">(更新轉知)臺南市政府開設臺南園區大型疫苗接種站</div>
+                </div>
+                <div class="bottom">2021-10-19 14:07:34</div>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
